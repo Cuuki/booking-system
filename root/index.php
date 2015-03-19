3f6411881e9d6a578e177fc6a591f99a4f3ee6eb
Patrick Soellner <patrick.soellner@publicis.de>
2015-03-19 09:39:50 +0100

    Add symfony/validator

--- root/index.php
+++ root/index.php
@@ -12,6 +12,7 @@ $app = new Silex\Application();
 
 $app->register( new Silex\Provider\UrlGeneratorServiceProvider() );
 $app->register( new Silex\Provider\SessionServiceProvider() );
+$app->register( new Silex\Provider\ValidatorServiceProvider() );
 $app->register( new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__ . '/../templates'
 ) );
